dc22ca3ac9415532f4c2bba21a1d47e7f3c9e560
Loan LEPINEY <llepiney@f4r3s2.clusters.42paris.fr>
2026-01-20 16:37:55 +0100

    Added comments on boot.s

--- src/boot.s
+++ src/boot.s
@@ -1,33 +1,37 @@
+/* This is the asembly code that is the entry point of the kernel : it sets things
+up so the C code can execute (kernel_main call)*/
+
 /* Multiboot header constants */
-.set ALIGN,    1<<0             /* align loaded modules on page boundaries */
-.set MEMINFO,  1<<1             /* provide memory map */
-.set FLAGS,    ALIGN | MEMINFO  /* Multiboot flag field */
-.set MAGIC,    0x1BADB002       /* magic number for bootloader */
-.set CHECKSUM, -(MAGIC + FLAGS) /* checksum to prove we are multiboot */
+.set ALIGN,    1<<0             // (align flag) align loaded modules on page boundaries (starts at a memory block)
+.set MEMINFO,  1<<1             // asks memory map to bootloader
+.set FLAGS,    ALIGN | MEMINFO
+.set MAGIC,    0x1BADB002       // magic number to indicate multiboot to bootloader */
+.set CHECKSUM, -(MAGIC + FLAGS) // integrity check
 
 /* Multiboot header section */
 .section .multiboot
-.align 4
+.align 4                // align on 4 octets boundaries
+// writes the following raw memory
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
 
 /* Stack section */
 .section .bss
-.align 16
+.align 16                       // align on 16 octets boundaries
 stack_bottom:
-.skip 16384                     /* 16 KB stack */
+.skip 16384                     // 16 KB stack reserved for uninitialized data
 stack_top:
 
 /* Text section */
 .section .text
-.global _start
-.type _start, @function
+.global _start                  // we precise the linking of _start
+.type _start, @function         // we precise the type of _start
 _start:
-    mov $stack_top, %esp
+    mov $stack_top, %esp        // Initializes stack pointer on %esp (stack registry)
 
     call kernel_main
 
-    cli
-1:  hlt
-    jmp 1b
+    cli                         // disables interrupts (because we can't handle them yet)
+1:  hlt                         // "1" is a label (sort of variable) that is reusable in other scopes, "hlt" for halt of the CPU
+    jmp 1b                      // "1b" label for "1 backward", so we jump back if something rwakes up
